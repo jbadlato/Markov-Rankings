ee3a4657a2606577c9d5f6e65ea2ca243ef8154e
Joseph Badlato <jbadlato@vt.edu>
2017-10-05 15:53:58 -0400

    Added a 'learn more' page to explain calculations

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
 
-// views is directory for all template files
-app.set('views', __dirname);
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-	response.render('index');
-});
-
 app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
 });
+
+app.get('/', function(request, response) {
+	response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/learn-more', function(request, response) {
+	response.sendFile(__dirname + '/views/learn_more.html')
+})
+
 
 function sendRankings(res, league) {
 	const client = new Client({

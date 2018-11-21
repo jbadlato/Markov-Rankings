46dc48d144bc8bc2c521cfe478cbc1e7e6caddf4
Joseph Badlato <jbadlato@vt.edu>
2018-11-21 13:54:20 -0500

    Created directory for Version 2 & added design plan & DDL

---
+++ Version 2/Database/DDL/Tables_DDL.sql
@@ -0,0 +1,65 @@
+CREATE TABLE league (
+	id INTEGER,
+	league_name VARCHAR(4),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE season (
+	id INTEGER,
+	league_id INTEGER,
+	season INTEGER,	-- Year
+	teams_url VARCHAR(255),
+	scores_url VARCHAR(255),
+	week_start INTEGER,	-- 0-6, Sunday - Saturday
+	season_start DATE,
+	season_end DATE,
+	PRIMARY KEY (id),
+	FOREIGN KEY (league_id) REFERENCES league (id)
+);
+
+CREATE TABLE team (
+	id INTEGER,
+	name VARCHAR(20),
+	season_id INTEGER,
+	twitter_handle VARCHAR(15),
+	logo_file VARCHAR(255),
+	win_count INTEGER,
+	loss_count INTEGER,
+	tie_count INTEGER,
+	PRIMARY KEY (id, season_id),
+	FOREIGN KEY (season_id) REFERENCES season (id)
+);
+
+CREATE TABLE score (
+	id INTEGER,
+	season_id INTEGER,
+	game_date DATE,
+	team_id INTEGER,
+	score INTEGER,
+	opponent_id INTEGER,
+	scheduled_ind INTEGER,
+	home_ind INTEGER,
+	PRIMARY KEY (id),
+	FOREIGN KEY (season_id) REFERENCES season (id),
+	FOREIGN KEY (team_id) REFERENCES team (id),
+	FOREIGN KEY (opponent_id) REFERENCES team (id)
+);
+
+CREATE TABLE rank (
+	id INTEGER,
+	team_id INTEGER,
+	week_number INTEGER,
+	rank INTEGER,
+	source VARCHAR(255),
+	PRIMARY KEY (id),
+	FOREIGN KEY (team_id) REFERENCES team (id)
+);
+
+CREATE TABLE prediction (
+	id INTEGER,
+	score_id INTEGER,
+	week_number INTEGER,
+	prediction INTEGER,
+	PRIMARY KEY (id),
+	FOREIGN KEY (score_id) REFERENCES score (id)
+);

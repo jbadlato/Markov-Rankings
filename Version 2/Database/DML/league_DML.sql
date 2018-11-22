fa3e8620402441d4cff0b317d85d336a3a61eb0d
Joseph Badlato <jbadlato@vt.edu>
2018-11-21 20:58:30 -0500

    Added league DML

---
+++ Version 2/Database/DML/league_DML.sql
@@ -0,0 +1,6 @@
+INSERT INTO league (name) VALUES ('CBB');
+INSERT INTO league (name) VALUES ('CFB');
+INSERT INTO league (name) VALUES ('NFL');
+INSERT INTO league (name) VALUES ('NHL');
+INSERT INTO league (name) VALUES ('WCBB');
+INSERT INTO league (name) VALUES ('NBA');

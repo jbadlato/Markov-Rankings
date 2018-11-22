3635e3480b7d8873851570d92f6b3a493d3d6a07
Joseph Badlato <jbadlato@vt.edu>
2018-11-21 20:59:59 -0500

    Added season DML

---
+++ Version 2/Database/DML/season_DML.sql
@@ -0,0 +1,18 @@
+INSERT INTO season (
+		league_id,
+		season,
+		teams_url,
+		scores_url,
+		week_start,
+		season_start,
+		season_end
+	)
+	VALUES (
+			1,
+			2019,
+			'https://www.masseyratings.com/scores.php?s=305972&sub=11590&all=1&mode=2&format=2',
+			'https://www.masseyratings.com/scores.php?s=305972&sub=11590&all=1&mode=2&format=1',
+			1,
+			DATE '2018-11-06',
+			DATE '2019-04-08'
+		);

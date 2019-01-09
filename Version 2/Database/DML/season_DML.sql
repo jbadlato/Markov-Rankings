953c1740acb4ad18d4b135a973a6b4f597096aac
Joseph Badlato <jbadlato@vt.edu>
2019-01-08 20:11:57 -0500

    Updated DML for scores link--now includes scheduled games

--- Version 2/Database/DML/season_DML.sql
+++ Version 2/Database/DML/season_DML.sql
@@ -10,8 +10,8 @@ INSERT INTO season (
 	VALUES (
 			1,
 			2019,
-			'https://www.masseyratings.com/scores.php?s=305972&sub=11590&all=1&mode=2&format=2',
-			'https://www.masseyratings.com/scores.php?s=305972&sub=11590&all=1&mode=2&format=1',
+			'https://www.masseyratings.com/scores.php?s=305972&sub=11590&all=1&mode=2&sch=on&format=2',
+			'https://www.masseyratings.com/scores.php?s=305972&sub=11590&all=1&mode=2&sch=on&format=1',
 			1,
 			DATE '2018-11-06',
 			DATE '2019-04-08'

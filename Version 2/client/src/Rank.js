edab0a18984804b459133aa87d52f80a771c2f0e
Joseph Badlato <jbadlato@vt.edu>
2019-02-03 12:34:41 -0500

    Added win-loss records to ranking list, adjusted CSS

--- Version 2/client/src/Rank.js
+++ Version 2/client/src/Rank.js
@@ -2,28 +2,30 @@ import React, { Component } from 'react';
 
 class Rank extends Component {
   render() {
+  	let record = '(' + this.props.rank.win_count + '-' + this.props.rank.loss_count;
+  	if (this.props.rank.tie_count > 0) {
+  		record += '-' + this.props.rank.tie_count;
+  	}
+  	record += ')';
   	return (
   		<div className="container rank-container">
-  			<div className="row">
-  				<div className="col-sm-1">
+  			<div className="row rank-row">
+  				<div className="col-xs-1">
   					<h1>{this.props.rank.rank}</h1>
   				</div>
-  				<div className="col-sm-3">
-  					<img src={'client/public/img/teams/' + this.props.rank.team_logo_file} alt={this.props.rank.team_name.split('_').join(' ')} />
+  				<div className="col-xs-3">
+  					<img className="team-logo" src={'client/public/img/teams/' + this.props.rank.team_logo_file} alt={this.props.rank.team_name.split('_').join(' ')} />
   				</div>
-  				<div className="col-sm-6">
+  				<div className="col-xs-4">
   					<h1>{this.props.rank.team_name.split('_').join(' ')}</h1>
   				</div>
-  				<div className="col-sm-2">
-  					<img className="conference-logo" src={'client/public/img/conferences/' + this.props.rank.conference_logo_file} alt={this.props.rank.conference_name} />
+  				<div className="col-xs-2">
+  					<h3>{record}</h3>
+  				</div>
+  				<div className="col-xs-2 rank-conference-logo-container">
+  					<img className="conference-logo pull-right" src={'client/public/img/conferences/' + this.props.rank.conference_logo_file} alt={this.props.rank.conference_name} />
   				</div>
 				</div>
-				<div className="row">
-  				<div className="col-sm-4">
-  					<h3>Rating: {(this.props.rank.rating * 10000).toFixed(2)}</h3>
-  				</div>
-  				<div className="col-sm-8"></div>
-  			</div>
   		</div>
   		);
   }

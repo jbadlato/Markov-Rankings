46bf86470e513861b32a26aec3c0a010e998fe55
Joseph Badlato <jbadlato@vt.edu>
2019-03-03 14:44:55 -0500

    Updated server to serve react app from build file

--- v2/server.js
+++ v2/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 5000;
 let { Client } = require('pg');
@@ -18,14 +19,14 @@ client.connect((err)=> {
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'client/build')));
 
 app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
 });
 
 app.get('/', function(request, response) {
-	
+	response.sendFile(path.join(__dirname,'/client/build/index.html'));
 });
 
 app.get('/api/league/:id/ranks', async function(request, response) {

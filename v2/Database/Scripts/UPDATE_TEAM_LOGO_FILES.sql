1a6df7df8de27823ab78458c9428007131f67736
Joseph Badlato <jbadlato@vt.edu>
2019-03-30 18:17:48 -0400

    Updated NHL team logo file names in database script

--- v2/Database/Scripts/UPDATE_TEAM_LOGO_FILES.sql
+++ v2/Database/Scripts/UPDATE_TEAM_LOGO_FILES.sql
@@ -2,6 +2,7 @@
 UPDATE team
 	SET logo_file = lower(replace(replace(name, '''', ''), '.', ''))||'.png'
 	WHERE season_id = 1 OR season_id = 5;
+-- NBA
 UPDATE team
 	SET logo_file = 'phoenix_suns.png'
 	WHERE name = 'Phoenix'
@@ -122,8 +123,124 @@ UPDATE team
 	SET logo_file = 'milwaukee_bucks.png'
 	WHERE name = 'Milwaukee'
 		AND season_id = 6;
-		
-		
-		
-		
-		
+-- NHL
+UPDATE team
+	SET logo_file = 'calgary_flames.png'
+	WHERE name = 'Calgary'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'anaheim_ducks.png'
+	WHERE name = 'Anaheim'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'los_angeles_kings.png'
+	WHERE name = 'Los_Angeles'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'chicago_blackhawks.png'
+	WHERE name = 'Chicago'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'minnesota_wild.png'
+	WHERE name = 'Minnesota'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'san_jose_sharks.png'
+	WHERE name = 'San_Jose'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'nashville_predators.png'
+	WHERE name = 'Nashville'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'colorado_avalanche.png'
+	WHERE name = 'Colorado'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'new_jersey_devils.png'
+	WHERE name = 'New_Jersey'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'vancouver_canucks.png'
+	WHERE name = 'Vancouver'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'columbus_blue_jackets.png'
+	WHERE name = 'Columbus'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'dallas_stars.png'
+	WHERE name = 'Dallas'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'pittsburgh_penguins.png'
+	WHERE name = 'Pittsburgh'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'philadelphia_flyers.png'
+	WHERE name = 'Philadelphia'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'winnipeg_jets.png'
+	WHERE name = 'Winnipeg'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'new_york_islanders.png'
+	WHERE name = 'NY_Islanders'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'arizona_coyotes.png'
+	WHERE name = 'Arizona'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'edmonton_oilers.png'
+	WHERE name = 'Edmonton'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'ottawa_senators.png'
+	WHERE name = 'Ottawa'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'vegas_golden_knights.png'
+	WHERE name = 'Vegas'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'detroit_red_wings.png'
+	WHERE name = 'Detroit'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'carolina_hurricanes.png'
+	WHERE name = 'Carolina'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'montreal_canadiens.png'
+	WHERE name = 'Montreal'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'buffalo_sabres.png'
+	WHERE name = 'Buffalo'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'toronto_maple_leafs.png'
+	WHERE name = 'Toronto'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'new_york_rangers.png'
+	WHERE name = 'NY_Rangers'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'florida_panthers.png'
+	WHERE name = 'Florida'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'boston_bruins.png'
+	WHERE name = 'Boston'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'washington_capitals.png'
+	WHERE name = 'Washington'
+		AND season_id = 4;
+UPDATE team
+	SET logo_file = 'tampa_bay_lightning.png'
+	WHERE name = 'Tampa_Bay'
+		AND season_id = 4;

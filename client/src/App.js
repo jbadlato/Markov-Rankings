9a16f3a008a22b0e18dbea55d593d93951a185bc
jbadlato <joebadlato@gmail.com>
2019-04-14 16:47:30 -0400

    Redirect user to latest ranks for the specified league/season if the input date is later than the last ranking calculation

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter as Router,
+  Router,
   Route,
   Switch
 } from 'react-router-dom';
+import history from './history.js';
 import Home from './Home.js';
 import RankingsContainer from './RankingsContainer.js';
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/:league_name/:season/ranks/:date" component={RankingsContainer} />
--- client/src/RankingsContainer.js
+++ client/src/RankingsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import RankList from './RankList.js';
+import history from './history.js';
 
 class RankingsContainer extends Component {
   constructor(props) {
@@ -10,9 +11,20 @@ class RankingsContainer extends Component {
   }
 
   componentDidMount() {
+ 	fetch('/api/' + this.props.match.params.league_name + '/' + this.props.match.params.season + '/latest_rank_date')
+		.then(response => response.json())
+		.then(data => {
+			let latestRankdate = data.rows[0].latest_rank_date;
+			if (latestRankdate < this.props.match.params.date) {
+				console.log('Redirecting...');
+				history.push('/'+this.props.match.params.league_name+'/'+this.props.match.params.season+'/ranks/'+latestRankdate);
+			} else console.log('Did not redirect');
+		}
+		).then(() => { 
     fetch('/api/' + this.props.match.params.league_name + '/' + this.props.match.params.season + '/ranks/' + this.props.match.params.date)
       .then(response => response.json())
       .then(data => this.setState({ rankings: data.rows}));
+		});
   }
 
   render() {

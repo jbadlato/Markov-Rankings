6fd0c2d263d36834193f83ada9b5605aed2766aa
jbadlato <joebadlato@gmail.com>
2019-04-14 16:59:14 -0400

    Cleaned up

--- client/src/RankingsContainer.js
+++ client/src/RankingsContainer.js
@@ -16,14 +16,13 @@ class RankingsContainer extends Component {
 		.then(data => {
 			let latestRankdate = data.rows[0].latest_rank_date;
 			if (latestRankdate < this.props.match.params.date) {
-				console.log('Redirecting...');
 				history.push('/'+this.props.match.params.league_name+'/'+this.props.match.params.season+'/ranks/'+latestRankdate);
-			} else console.log('Did not redirect');
-		}
-		).then(() => { 
-    fetch('/api/' + this.props.match.params.league_name + '/' + this.props.match.params.season + '/ranks/' + this.props.match.params.date)
-      .then(response => response.json())
-      .then(data => this.setState({ rankings: data.rows}));
+			}
+		})
+		.then(() => {
+			fetch('/api/' + this.props.match.params.league_name + '/' + this.props.match.params.season + '/ranks/' + this.props.match.params.date)
+			  .then(response => response.json())
+			  .then(data => this.setState({ rankings: data.rows}));
 		});
   }
 

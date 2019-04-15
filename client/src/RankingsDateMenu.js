2148194091d2fa69b29f19ecdc2685ddf174b088
jbadlato <joebadlato@gmail.com>
2019-04-14 21:13:14 -0400

    Added required dependencies for styling of the bootstrap dropdown menu

--- client/src/RankingsDateMenu.js
+++ client/src/RankingsDateMenu.js
@@ -4,6 +4,8 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 class RankingsDateMenu extends Component {
   constructor(props) {

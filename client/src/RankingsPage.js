2540371990ea64cde8281a5df57fb28be17eb8fe
jbadlato <joebadlato@gmail.com>
2019-04-16 21:16:13 -0400

    Show date calculated as h4 element. Testing codeship automatic deployment

--- client/src/RankingsPage.js
+++ client/src/RankingsPage.js
@@ -13,9 +13,9 @@ class HomePage extends Component {
 			  </h1>
 			</div>
 			<div className="row">
-				<p className="col-xs-3">Showing rankings calculated on: {this.props.match.params.date.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3')}</p>
+				<h4 className="col-xs-5">Showing rankings calculated on: {this.props.match.params.date.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3')}</h4>
 				<RankingsDateMenu league_name={this.props.match.params.league_name} season={this.props.match.params.season} className="col-xs-2" />
-				<div className="col-xs-7" />
+				<div className="col-xs-5" />
 			</div>
 		</div>
         <RankingsContainer league_name={this.props.match.params.league_name} season={this.props.match.params.season} date={this.props.match.params.date} />

5d8a682b980d7ea6b364da6e589566598e357c75
jbadlato <joebadlato@gmail.com>
2019-04-21 12:12:49 -0400

    Changed game list to rows of 2

--- client/src/Game.js
+++ client/src/Game.js
@@ -37,39 +37,41 @@ class Game extends Component {
 		awayTeam = new Team(this.props.game.opponent_name, opponentRecord, this.props.game.opponent_logo_file, this.props.game.opponent_score, this.props.game.opponent_conference_name, this.props.game.opponent_conference_logo);
 	}
   	return (
-  		<div className="container game-container">
-			<div>
-				<h1>{this.props.game.game_date}</h1>
+		<div className="list-group-item w-50 list-group-item-action">
+			<div className="container game-container">
+				<div>
+					<h1>{this.props.game.game_date}</h1>
+				</div>
+				<div className="row away-row">
+					<div className="col-xs-3">
+						<img className="team-logo" src={'/img/teams/' + awayTeam.logoFile} title={awayTeam.name.split('_').join(' ')} alt={awayTeam.name.split('_').join(' ')} />
+					</div>
+					<div className="col-xs-5">
+						<h1>{awayTeam.name.split('_').join(' ')}</h1>
+					</div>
+					<div className="col-xs-2">
+						<h1>{awayTeam.record}</h1>
+					</div>
+					<div className="col-xs-1">
+						<h1>{awayTeam.score}</h1>
+					</div>
+				</div>
+				<div className="row home-row">
+					<div className="col-xs-3">
+						<img className="team-logo" src={'/img/teams/' + homeTeam.logoFile} title={homeTeam.name.split('_').join(' ')} alt={homeTeam.name.split('_').join(' ')} />
+					</div>
+					<div className="col-xs-5">
+						<h1>{homeTeam.name.split('_').join(' ')}</h1>
+					</div>
+					<div className="col-xs-2">
+						<h1>{homeTeam.record}</h1>
+					</div>
+					<div className="col-xs-1">
+						<h1>{homeTeam.score}</h1>
+					</div>
+				</div>
 			</div>
-  			<div className="row away-row">
-				<div className="col-xs-3">
-					<img className="team-logo" src={'/img/teams/' + awayTeam.logoFile} title={awayTeam.name.split('_').join(' ')} alt={awayTeam.name.split('_').join(' ')} />
-				</div>
-				<div className="col-xs-5">
-					<h1>{awayTeam.name.split('_').join(' ')}</h1>
-				</div>
-				<div className="col-xs-2">
-					<h1>{awayTeam.record}</h1>
-				</div>
-				<div className="col-xs-1">
-					<h1>{awayTeam.score}</h1>
-				</div>
-			</div>
-			<div className="row home-row">
-				<div className="col-xs-3">
-					<img className="team-logo" src={'/img/teams/' + homeTeam.logoFile} title={homeTeam.name.split('_').join(' ')} alt={homeTeam.name.split('_').join(' ')} />
-				</div>
-				<div className="col-xs-5">
-					<h1>{homeTeam.name.split('_').join(' ')}</h1>
-				</div>
-				<div className="col-xs-2">
-					<h1>{homeTeam.record}</h1>
-				</div>
-				<div className="col-xs-1">
-					<h1>{homeTeam.score}</h1>
-				</div>
-			</div>
-  		</div>
+		</div>
   		);
   }
 }
--- client/src/Schedule.js
+++ client/src/Schedule.js
@@ -4,9 +4,15 @@ import Game from './Game.js';
 class RankList extends Component {
   render() {
   	return (
-  		<div id="schedule">
-  			{this.props.schedule.map(game => <Game key={game.game_date + game.opponent_name} game={game} />)}
-  		</div>
+      <div className="container">
+		<div className="row">
+			<div className="col-12">
+				<div className="list-group d-flex flex-row flex-wrap">
+					{this.props.schedule.map(game => <Game key={game.game_date + game.opponent_name} game={game} />)}
+				</div>
+			</div>
+		</div>
+      </div>
   		);
   }
 }

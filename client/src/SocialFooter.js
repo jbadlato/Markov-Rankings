7b31bec977c70cce736da89d869a6e07e7daa277
jbadlato <joebadlato@gmail.com>
2019-05-04 15:36:07 -0400

    Added footer with twitter and github links

---
+++ client/src/SocialFooter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class SocialFooter extends Component {
+  render() {
+    return (
+<div className="box">
+    <div className="container">
+     	<div className="row">
+			<div className="col-lg-4"></div>
+			<a href="https://twitter.com/markovrankings" className="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+				<div className="box-part text-center box-twitter">
+					<i className="fa fa-twitter fa-5x" aria-hidden="true"></i>
+				 </div>
+			</a>	 
+			<a href="https://github.com/jbadlato/Markov-Rankings" className="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+				<div className="box-part text-center box-github">
+					<i className="fa fa-github fa-5x" aria-hidden="true"></i>
+				 </div>
+			</a>
+			<div className="col-lg-4"></div>
+		</div>		
+    </div>
+</div>
+    );
+  }
+}
+
+export default SocialFooter;

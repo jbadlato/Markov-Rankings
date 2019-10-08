c871a434a07b61003260f30c4a5161092bcc0822
jbadlato <joebadlato@gmail.com>
2019-10-08 19:06:45 -0400

    Fix scheduled indicator

--- Database/Scripts/UPDATE_SCHEDULED_IND.sql
+++ Database/Scripts/UPDATE_SCHEDULED_IND.sql
@@ -3,7 +3,7 @@ BEGIN
 	UPDATE score 
 		SET scheduled_ind = 
 		CASE 
-			WHEN game_date < DATE(CURRENT_DATE AT TIME ZONE 'US/Pacific') THEN 0 
+			WHEN game_date <= DATE(CURRENT_DATE AT TIME ZONE 'US/Pacific') THEN 0 
 			ELSE 1 
 		END;
 	RAISE INFO 'Updated scheduled indicators.';

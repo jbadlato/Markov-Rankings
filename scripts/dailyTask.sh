f6befc50e70c843eedfd6ea39995708881ef13d0
Joe Badlato <joebadlato@gmail.com>
2020-12-05 18:24:59 -0500

    Remove strict promise rejection

--- scripts/dailyTask.sh
+++ scripts/dailyTask.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"/..
-node --unhandled-rejections=strict ./build/scripts/ingest.js
+node ./build/scripts/ingest.js
 psql $BACKEND_DATABASE_URL < ./db/scripts/UPDATE_TEAM_CONFERENCES.sql
 psql $BACKEND_DATABASE_URL < ./db/scripts/UPDATE_TEAM_LOGO_FILES.sql
 psql $BACKEND_DATABASE_URL < ./db/scripts/UPDATE_SCHEDULED_IND.sql
 psql $BACKEND_DATABASE_URL < ./db/scripts/DELETE_CANCELLED_GAMES.sql
 psql $BACKEND_DATABASE_URL < ./db/scripts/UPDATE_WIN_LOSS_RECORDS.sql
-node --unhandled-rejections=strict ./build/scripts/calculateRankings.js
+node ./build/scripts/calculateRankings.js
